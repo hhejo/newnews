42a3b418f4c6236cb7bfb9843029dc38e399d03b
hhejo <litsmainjof@gmail.com>
2024-09-23 07:12:33 +0900

    feat: update item

--- components/book-section-item.tsx
+++ components/book-section-item.tsx
@@ -2,23 +2,30 @@ import { Book } from '@/type';
 
 export default function BookSectionItem({ book }: { book: Book }) {
   return (
-    <li className="rounded-xl border border-gray-100 overflow-hidden w-64 flex flex-col">
-      <img
-        className="max-w-64"
-        src={book.cover}
-        loading="lazy"
-        alt="책 커버 이미지"
-      />
+    // 책 카드
+    <li className="rounded-xl border border-gray-100 overflow-hidden w-64 shadow-lg">
+      {/* 카드 이미지 */}
+      <div className="relative overflow-hidden">
+        <div
+          className="absolute inset-0 bg-cover bg-center brightness-50 blur-sm"
+          style={{ backgroundImage: `url('${book.cover}')` }}
+        ></div>
+        <img
+          className="relative mx-auto max-w-64 object-cover"
+          src={book.cover}
+          loading="lazy"
+          alt="책 커버 이미지"
+        />
+      </div>
+      {/* 카드 내용 */}
       <div className="p-2 flex flex-col gap-4">
         <h3 className="font-bold text-lg text-gray-800">{book.title}</h3>
-        <div className="text-sm text-gray-400">
+        <div className="text-sm text-gray-400 text-end">
           <span>{book.author}</span>
           <div className="w-px bg-gray-300"></div>
           <span>{book.publisher}</span>
           <div className="w-px bg-gray-300"></div>
-          {/* <span className="align-middle">{book.pubDate}</span> */}
         </div>
-        {/* <p>{book.description}</p> */}
       </div>
     </li>
   );
