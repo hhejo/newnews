732bcc8e929852582695496cf272c322d2a4e28b
hhejo <litsmainjof@gmail.com>
2024-09-23 16:53:20 +0900

    feat: update book section card end

--- components/book-section.tsx
+++ components/book-section.tsx
@@ -7,7 +7,7 @@ export default function BookSection({ books }: { books: Book[] }) {
   return (
     <section className="flex flex-col gap-14 py-20">
       <h2 className="font-bold text-3xl text-center">🥳 새로 나온 책들</h2>
-      <ul className="flex flex-wrap justify-center gap-8">
+      <ul className="flex flex-wrap justify-center items-end gap-8">
         {books.map((book) => (
           <Link key={book.itemId} href={`/books/${book.isbn13}`}>
             <BookSectionItem book={book} />
