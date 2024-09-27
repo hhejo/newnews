c2cfcc7778e28593c732db6238d19a8e085cdc75
hhejo <litsmainjof@gmail.com>
2024-09-27 17:30:00 +0900

    chore: remove console

--- components/book-section.tsx
+++ components/book-section.tsx
@@ -3,7 +3,6 @@ import BookSectionItem from './book-section-item';
 import Link from 'next/link';
 
 export default function BookSection({ books }: { books: Book[] }) {
-  console.log('books:', books);
   return (
     <section className="flex flex-col gap-14 py-20">
       <h2 className="font-bold text-3xl text-center">🥳 새로 나온 책들</h2>
