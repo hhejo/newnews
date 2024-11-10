625dcb271ae1e2aaf1552506fbc5b711cd78c783
hhejo <litsmainjof@gmail.com>
2024-11-10 21:15:39 +0900

    feat: update store

--- store/use-store.ts
+++ store/use-store.ts
@@ -1,8 +1,8 @@
-import { create } from 'zustand';
+// import { create } from 'zustand';
 
-export const useStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
-}));
+// export const useStore = create((set) => ({
+//   bears: 0,
+//   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+//   removeAllBears: () => set({ bears: 0 }),
+//   updateBears: (newBears) => set({ bears: newBears }),
+// }));
