937e915e7cc02ff50e93824ff9edb6ea6b2dae13
hhejo <litsmainjof@gmail.com>
2024-11-10 20:48:32 +0900

    feat: add store test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+import { useStore } from '@/store/use-store';
+
 export default function Home() {
+  const bears = useStore((state) => state.bears);
+
   return (
-    <main className="container mx-auto flex flex-col gap-16">
-      랜딩 페이지 / 홈 페이지
-    </main>
+    <>
+      <main className="container mx-auto flex flex-col gap-16">랜딩 페이지 / 홈 페이지</main>
+      {bears} around here...
+      <br />
+      <Controls />
+    </>
   );
+}
+
+function Controls() {
+  const increasePopulation = useStore((state) => state.increasePopulation);
+  return <button onClick={increasePopulation}>one up</button>;
 }
